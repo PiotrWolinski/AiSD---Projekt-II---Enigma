a42bab22be2dcb1f0f663a2d80b3b5d3f8ea22d4
PiotrWolinski <55030310+PiotrWolinski@users.noreply.github.com>
2020-03-18 21:59:01 +0100

    Nothing was improved

--- main.cpp
+++ main.cpp
@@ -5,40 +5,38 @@
 #include <iostream>
 
 struct Rotor {
-	unsigned int n=0;				 //wielkosc alfabetu
-	unsigned int* perm;				 //tablica do przechowywania jego permutacji
-	unsigned int notch_n=0;			 //ilosc liter powodujacych obrot innych wirnikow
-	unsigned int* notch;			 //tablica do przechowywania tych liter
-	unsigned int t_n = 0;			 //liczba obrotow, ktore wykonal rotor od swojego pierwotnego polozenia
+	short* perm;						//tablica do przechowywania jego permutacji
+	short notch_n;						//ilosc liter powodujacych obrot innych wirnikow
+	short* notch;						//tablica do przechowywania tych liter
+	short t_n;							//liczba obrotow, ktore wykonal rotor od swojego pierwotnego polozenia
 };
 
 struct Reflector {
-	unsigned int n = 0;				//wielkosc alfabetu
-	unsigned int* perm;				//tablica do przechowywania jego permutacji
+	short* perm;						//tablica do przechowywania jego permutacji
 };
 
 struct Machine {
-	unsigned int n = 0;				//wielkosc alfabetu
-	unsigned int m = 0;				//liczba wirnikow
-	unsigned int l = 0;				//liczba reflektorow
-	Rotor* rotors;					//tablica przechowujaca rotory
-	Reflector* reflectors;			//tablica przechowujaca reflektory
+	short n;							//wielkosc alfabetu
+	short m;							//liczba wirnikow
+	short l;							//liczba reflektorow
+	Rotor* rotors;						//tablica przechowujaca rotory
+	Reflector* reflectors;				//tablica przechowujaca reflektory
 };
 
-struct Task {
-	unsigned int p = 0;				//liczba zadan do rozwiazania
-	unsigned int ro_n = 0;			//liczba obcenie uzywanych rotorow
-	unsigned int* ro_id;			//tablica indeksow obecnie uzywanych rotorow
-	unsigned int* s_pos;			//tablica pozycji startowa rotora
-	unsigned int re_n = 0;			//indeks obecnie uzywanego reflektora
-	unsigned int msg_size = 2;		//rozmiar tablicy, ktora zawiera zdanie do zaszyfrowania
-	unsigned int* msg;				//tablica do przechowywania wiadomosci do zakodowania
+struct Task {		
+	short p;							//liczba zadan do rozwiazania
+	short ro_n;							//liczba obcenie uzywanych rotorow
+	short* ro_id;						//tablica indeksow obecnie uzywanych rotorow
+	short* s_pos;						//tablica pozycji startowa rotora
+	short re_n;							//indeks obecnie uzywanego reflektora
+	short msg_size;						//rozmiar tablicy, ktora zawiera zdanie do zaszyfrowania
+	short* msg;							//tablica do przechowywania wiadomosci do zakodowania
 };
 
 //przesuwa KOPIE ROTORA na pozycje startowa okreslona w konfiguracji zadan
-unsigned int* rotate_to_s_pos(unsigned int n, unsigned int* perm, int s_pos)
+short* rotate_to_s_pos(short n, short* perm,short s_pos)
 {
-	unsigned int* pom = (unsigned int*)malloc(n * sizeof(unsigned int));
+	short* pom = (short*)malloc(n * sizeof(short));
 	for (int i = 0; i < n; i++)
 	{
 		pom[i] = perm[(i + s_pos - 1) % n];
@@ -46,9 +44,9 @@ unsigned int* rotate_to_s_pos(unsigned int n, unsigned int* perm, int s_pos)
 	return pom;
 }
 
-unsigned int* rotate_before(unsigned int n, unsigned int* perm)
+short* rotate(short n, short* perm)
 {
-	unsigned int* pom = (unsigned int*)malloc(n * sizeof(unsigned int));
+	short* pom = (short*)malloc(n * sizeof(short));
 	for (int i = 0; i < n; i++)
 	{
 		pom[i] = perm[(i + 1) % n];
@@ -59,148 +57,164 @@ unsigned int* rotate_before(unsigned int n, unsigned int* perm)
 //zwraca indeks liczby, ktora trzeba sprawdzic w kolejnym rotorze lub reflektorze
 //input - indeks liczby, ktora sie sprawdza
 //output - indeks liczby, ktora bedzie sprawdzana w kolejnym rotorze/reflektorze
-unsigned int return_index(unsigned int input, Rotor r)
+short return_index(short input, Rotor r, short n)
 {
-	input--;									//przez to bede mial tutaj taka liczbe, jakiego indeksu dokladnie szukam
-	unsigned int tmp = r.perm[input] - 1;		//zapisuje liczbe, ktorej odpowiadajacej liczby z a podst bede szukal
-	unsigned int index = 0;
+	input--;										//przez to bede mial tutaj taka liczbe, jakiego indeksu dokladnie szukam				
+													//zapisuje liczbe, ktorej odpowiadajacej liczby z a podst bede szukal
 
-	for (int i = 0; i < r.n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		if (tmp == (i + r.t_n) % r.n)
+		if (r.perm[input] - 1 == (i + r.t_n) % n)
 		{
-			index = i;
-			break;
+
+			return i + 1;
 		}
 	}
-	return index + 1;
+	return 0;
 }
 
-unsigned int return_index_r(unsigned int input, Rotor r)
+short return_index_r(short input, Rotor r, short n)
 {							
-	input = (input + r.t_n) % r.n;
-	unsigned int index = 0;
+	input = (input + r.t_n) % n;
 
-	for (int i = 0; i < r.n; i++)
+	for (int i = 0; i < n; i++)
 	{
-		if (input == r.perm[i]%r.n)
+		if (input == r.perm[i]%n)
 		{
-			index = i;
-			break;
+			return i + 1;
 		}
 	}
-	return index + 1;
+	return 0;
 }
 
 //input - indeks liczby, ktora wchodzi do reflektora						
 //output - indeks, pod ktorym trzeba sprawdzic alfabet podstawowy kolejnego wirnika
-unsigned int return_index(unsigned int input, Reflector r)
+short return_index(short input, Reflector r)
 {
-	return  r.perm[input - 1];
+	return r.perm[input - 1];
 }
 
 void machine_config(Machine& machine)
 {
-	scanf("%u", &machine.n);
-	scanf("%u", &machine.m);
+	scanf("%hu", &machine.n);
+	scanf("%hu", &machine.m);
 	machine.rotors = (Rotor*)malloc(machine.m * sizeof(Rotor));
 
+	short j = 0;
+	short x = 0;
 	for (int i = 0; i < machine.m; i++)
 	{
-		machine.rotors[i].n = machine.n;
-		machine.rotors[i].perm = (unsigned int*)malloc(machine.n * sizeof(unsigned int));
-		int j = 0;
+		j = 0;
+		machine.rotors[i].t_n = 0;
+		machine.rotors[i].perm = (short*)malloc(machine.n * sizeof(short));
+
 		while (j < machine.n )
 		{
-			std::cin >> machine.rotors[i].perm[j];
+			scanf("%hu ", &machine.rotors[i].perm[j]);
 			j++;
 		}
-		scanf("%u", &machine.rotors[i].notch_n);
+		scanf("%hu", &machine.rotors[i].notch_n);
 		if (machine.rotors[i].notch_n)
 		{
-			machine.rotors[i].notch = (unsigned int*)malloc(machine.rotors[i].notch_n * sizeof(unsigned int));
-			int x = 0;
+			machine.rotors[i].notch = (short*)malloc(machine.rotors[i].notch_n * sizeof(short));
+			x = 0;
 			while (x < machine.rotors[i].notch_n)
 			{
-				std::cin >> machine.rotors[i].notch[x];
+				scanf("%hu", &machine.rotors[i].notch[x]);
 				x++;
 			}
 		}
 	}
 
-	scanf("%u", &machine.l);
+	scanf("%hu", &machine.l);
 	machine.reflectors = (Reflector*)malloc(machine.l * sizeof(Reflector));
 	for (int i = 0; i < machine.l; i++)
 	{
-		machine.reflectors[i].n = machine.n;
-		machine.reflectors[i].perm = (unsigned int*)malloc(machine.n * sizeof(unsigned int));
-		int j = 0;
+		machine.reflectors[i].perm = (short*)malloc(machine.n * sizeof(short)); // czy to "int" jest potrzebne?
+		j = 0;
 		while (j < machine.n)
 		{
-			std::cin >> machine.reflectors[i].perm[j];
+			scanf("%hu ", &machine.reflectors[i].perm[j]);
 			j++;
 		}
 	}
 }
 
-void generate_cipher(Task t, Machine machine)
+void generate_cipher(Task& t, Machine machine)
 {
-	Rotor* rotors = (Rotor*)malloc(t.ro_n * sizeof(Rotor));
+	Rotor* rotors = (Rotor*)malloc(t.ro_n * sizeof(Rotor));					//to jest chyba najwieksze gowno w calym kodzie
 	//przygotowanie rotorow - obracanie ich na pozycje startowe
 	for (int x = 0; x < t.ro_n; x++)
 	{
-		rotors[x].perm = rotate_to_s_pos(machine.rotors[t.ro_id[x]].n, machine.rotors[t.ro_id[x]].perm, t.s_pos[x]);
+		rotors[x].perm = rotate_to_s_pos(machine.n, machine.rotors[t.ro_id[x]].perm, t.s_pos[x]);
 		rotors[x].t_n = t.s_pos[x] - 1;
-		rotors[x].n = machine.n;
+		rotors[x].notch_n = machine.rotors[t.ro_id[x]].notch_n;
+		rotors[x].notch = machine.rotors[t.ro_id[x]].notch;
 	}
 
 	//kodowanie wlasciwej wiadomosci
 	for (int i = 0; i < t.msg_size; i++)
 	{
-		unsigned int tmp = t.msg[i];
+
 		for (int j = 0; j < t.ro_n; j++)
 		{
-			if (!j)
+			if (j == 0)
 			{
-				rotors[j].perm = rotate_before(rotors[j].n, rotors[j].perm);
+				rotors[j].perm = rotate(machine.n, rotors[j].perm);				//tutaj krzyczy access violation xD
 				rotors[j].t_n++;
 			}
-			tmp = return_index(tmp, rotors[j]); 
+			t.msg[i] = return_index(t.msg[i], rotors[j], machine.n);
+
+			if (rotors[j].notch_n && j < t.ro_n - 1)
+			{
+				for (int o = 0; o < rotors[j].notch_n; o++)
+				{
+					if (rotors[j].notch[o] == rotors[j].perm[0])
+					{
+						rotors[j + 1].perm = rotate(machine.n, rotors[j + 1].perm);
+						rotors[j + 1].t_n++;
+					}
+				}
+			}
 		}
-		tmp = return_index(tmp, machine.reflectors[t.re_n]);
+		t.msg[i] = return_index(t.msg[i], machine.reflectors[t.re_n]);
 		for (int j = t.ro_n - 1; j >= 0; j--)
 		{
-			tmp = return_index_r(tmp, rotors[j]);
+			t.msg[i] = return_index_r(t.msg[i], rotors[j], machine.n);
 		}
-		printf("%u ", tmp);			
+		printf("%hu ", t.msg[i]);
 	}
 	printf("\n");
+	free(rotors);
+	rotors = NULL;
 }
 
 void tasks(Machine& machine)
 {
-	Task t;
-	scanf("%u", &t.p);
-	for (int i = 0; i < t.p; i++)
+	Task* t = (Task*)malloc(sizeof(Task));
+	scanf("%hu", &t->p);
+	short x = 0;
+	for (int i = 0; i < t->p; i++)
 	{
-		t.msg_size = 1;
-		std::cin >> t.ro_n;
-		t.ro_id = (unsigned int*)malloc(t.ro_n * sizeof(unsigned int));
-		t.s_pos = (unsigned int*)malloc(t.ro_n * sizeof(unsigned int));
-		for (int j = 0; j < t.ro_n; j++)
-		{
-			std::cin >> t.ro_id[j];
-			std::cin >> t.s_pos[j];
-		}
-		std::cin >> t.re_n;
+		t->msg_size = 2;
+		scanf("%hu ", &t->ro_n);
+		t->ro_id = (short*)malloc(t->ro_n * sizeof(short));
+		t->s_pos = (short*)malloc(t->ro_n * sizeof(short));
 
-		t.msg = (unsigned int*)malloc(t.msg_size * sizeof(unsigned int)); //robie miejsce na dwie cyfry
-		unsigned int x = 0;
-		int tmp = 0;
-		while (std::cin >> t.msg[x])
+		for (int j = 0; j < t->ro_n; j++)
 		{
-			tmp = t.msg[x];
-			if (t.msg[x] == 0)
+			scanf("%hu ", &t->ro_id[j]);
+			scanf("%hu ", &t->s_pos[j]);
+		}
+		scanf("%hu ", &t->re_n);
+
+		t->msg = (short*)malloc(t->msg_size * sizeof(short));
+		
+		x = 0;
+
+		while (std::cin >> t->msg[x])
+		{
+			if (t->msg[x] == 0)
 			{
 				break;
 			}
@@ -208,16 +222,24 @@ void tasks(Machine& machine)
 			{
 				x++;
 			}
-			if (x == t.msg_size)
+			if (x == t->msg_size)
 			{
-				t.msg_size+=2;
-				t.msg = (unsigned int*)realloc(t.msg, t.msg_size * sizeof(unsigned int));
+				t->msg_size++;
+				t->msg = (short*)realloc(t->msg, t->msg_size * sizeof(short));
 			}
 		}
-		t.msg_size = x;
-		generate_cipher(t, machine);
+
+		t->msg_size = x;
+		generate_cipher(*t, machine);
+		free(t->msg);
+		free(t->ro_id);
+		free(t->s_pos);
+		t->msg = NULL;
+		t->ro_id = NULL;
+		t->s_pos = NULL;
 	}
-	free(t.msg);
+	free(t);
+	t = NULL;
 }
 
 int main()
